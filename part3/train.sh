22c3968ee83726d978feb127b945f9007473f52b
Hanhong Zhao <hh-zhao23@mails.tsinghua.edu.cn>
2024-11-16 12:15:51 -0500

    fix val acc zero issue

--- part3/train.sh
+++ part3/train.sh
@@ -1,6 +1,6 @@
 echo "model_type: $1"
 echo "dataset_dir: ./data/rnn/cot_binary_32_1000000_right"
-echo "output_dir: ./output_$1_$2_$3"
+echo "output_dir: ./output_$1_$2_$3_lr$4_batch$5"
 echo "model_config_path: ./configs/$2m_transformer.json"
 echo "batch_size: $5"
 echo "total_training_samples: $3"
@@ -8,7 +8,7 @@ echo "lr: $4"
 python3 rnn/train.py \
     --model_type $1 \
     --dataset_dir $HOME/lhz_nlp_data/rnn/cot_binary_32_1000000_right \
-    --output_dir ./output_$1_$2_$3 \
+    --output_dir ./output_$1_$2_$3_lr$4_batch$5 \
     --model_config_path ./configs/$2m_transformer.json \
     --batch_size $5 \
     --total_training_samples $3 \
